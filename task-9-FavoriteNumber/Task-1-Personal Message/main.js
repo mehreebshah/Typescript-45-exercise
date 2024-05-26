65fe6622a1630661504a47ae38b2a865ad49849e
Areeba shah <160355732+mehreebshah@users.noreply.github.com>
2024-05-26 10:40:43 -0700

    Delete task-9-FavoriteNumber/Task-1-Personal Message directory

--- task-9-FavoriteNumber/Task-1-Personal Message/main.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const PersonName = "Eric";
-console.log(`"Hello ${PersonName},would you like to learn some Python today?"`);
--- task-9-FavoriteNumber/Task-1-Personal Message/main.ts
+++
@@ -1,2 +0,0 @@
-const PersonName:string = "Eric";
-console.log(`"Hello ${PersonName},would you like to learn some Python today?"`);
